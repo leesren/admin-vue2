16c3f33145b9b08439f71b8ab04fc52145200158
less zenberge <leesren@foxmail.com>
2017-03-01 10:59:22 +0800

    登录修改

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,34 @@
 <template>
-    <div>   
+    <div>
         <nprogress-container></nprogress-container>
-        <h3>登录</h3>
-        <a href="#/dashboard">首页</a>
+        <div  class="auth-main">
+            <div class="auth-block">
+                <h3>登录</h3>
+                <a href="#/dashboard">首页</a>
+            </div>
+        </div>
     </div>
 </template>
+<style lang="scss" scoped>
+    .auth-main{
+        display: flex;
+        align-items: center;
+        height: 100%;
+        width: 100%;
+        position: absolute;
+        background: radial-gradient(ellipse at center, #3ea8cb 0, #256cb7 100%);
+    }
+    .auth-block{
+        width: 540px;
+        margin: 6.5% auto;
+        border-radius: 5px;
+        background: rgba(0,0,0,0.55);
+        color: #fff;
+        padding: 132px;
+        font-size: 26px;
+    }
+</style>
 <script>
-    export default{
+    export default {
     }
 </script>
